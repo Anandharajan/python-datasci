d68d7d8ff8fcfa6d44d0a976b82833293d16a97c
ramtin <ramtin@ramtin-VirtualBox.(none)>
2013-05-10 14:48:17 -0400

    term sentiment analysis implemented.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,62 @@
 import sys
-
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+import json
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sentiments_path = sys.argv[1]
+    tweets_path = sys.argv[2]
+
+    tweets = []
+    for line in open(tweets_path,'r').readlines():
+        tweet = json.loads(line)
+        if "text" in tweet.keys():
+            text = tweet["text"].encode('utf-8')
+            tweets.append(text)
+
+    sentiments = {}
+    for line in open(sentiments_path,'r').readlines():
+        term, score = line.split("\t") 
+        sentiments[term] = int(score)             
+
+    calc_sentiments(tweets, sentiments)
+
+
+def calc_sentiments(tweets, sentiments):
+
+    tweet_sentiments = []
+    term_sentiments = {}
+
+    for tweet in tweets:
+        tweet_score = 0
+
+        for word in tweet.split():
+            word_score = sentiments[word.lower()] if word.lower() in sentiments else 0
+            tweet_score += word_score
+
+            if word.lower() not in term_sentiments.keys():
+                term_sentiments[word.lower()] = word_score
+
+
+        tweet_sentiments.append([tweet,tweet_score])
+
+
+    for term in term_sentiments:
+
+        if term not in sentiments:
+
+            new_score = 0
+            frequency = 1
+
+            for i in range(len(tweet_sentiments)):
+                if term in tweet_sentiments[i][0]:
+                    new_score += tweet_sentiments[i][1]
+                    frequency += 1
+
+            new_score /= frequency
+            term_sentiments[term.lower()] = new_score
+
+        print term+" "+str(float(term_sentiments[term]))
+
+
 
 if __name__ == '__main__':
     main()
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -7,32 +7,24 @@ def main():
     sentiments_path = sys.argv[1]
     tweets_path = sys.argv[2]
 
-    tweets = open(tweets_path,'r')
-    sentiments = dict()
+
+    sentiments = {}
 
     for line in open(sentiments_path,'r').readlines():
         sentement = line.split("\t")
-        try:
-            sentiments[sentement[0]] = int(sentement[1])
-        except ValueError:
-            sentiments[sentement[0]] = 0
+        sentiments[sentement[0]] = int(sentement[1])
 
-    for line in tweets.readlines():
+    for line in open(tweets_path,'r').readlines():
         tweet = json.loads(line[:-1],encoding='utf-8')
-        try:
-            text = tweet['text']
-        except KeyError:
-            text = ""
+        text = tweet['text'] if "text" in tweet.keys() else ""
         text = re.sub('[^A-Za-z0-9 ]+','',text).lower().split(" ")
         print calc_sentiment_score(text,sentiments)
 
 def calc_sentiment_score(text,sentiments):
     score = 0
     for word in text:
-        try:
-            score += sentiments[word]
-        except KeyError:
-            score += 0
+        delta = sentiments[word.lower()] if word.lower() in sentiments.keys() else 0
+        score += delta
     return score
 
 if __name__ == '__main__':
